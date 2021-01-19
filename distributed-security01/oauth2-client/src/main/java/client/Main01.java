3d675a0fba51ca7f9afbc3b4890abac262af3d51
xiluhua <505440@163.com>
2021-01-19 15:16:00 +0800

    add oauth2-client

---
+++ distributed-security01/oauth2-client/src/main/java/client/Main01.java
@@ -0,0 +1,88 @@
+package client;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+public class Main01 {
+
+    public static void main(String[] args) {
+
+        Main01.doGetTestWayOne();
+    }
+
+    /**
+     * GET---有参测试 (方式一:手动在url后面加上参数)
+     *
+     * @date 2018年7月13日 下午4:19:23
+     */
+    public static void doGetTestWayOne() {
+
+
+        // 获得Http客户端(可以理解为:你得先有一个浏览器;注意:实际上HttpClient与浏览器是不一样的)
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        // 参数
+        StringBuffer params = new StringBuffer();
+
+        // 创建Get请求
+        HttpGet httpGet = new HttpGet("http://localhost:53021/order/r1/");
+        httpGet.setHeader("Authorization","Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsicmVzMSJdLCJ1c2VyX25hbWUiOiJ6aGFuZ3NhbiIsInNjb3BlIjpbImFsbCJdLCJleHAiOjE2MTEwNDUwNTIsImF1dGhvcml0aWVzIjpbInAxIiwicDIiLCJwMyJdLCJqdGkiOiJjNDE0MDBkMC04MWQ3LTRjNjAtYjkzOS02MmYxNWMyMzRhMzIiLCJjbGllbnRfaWQiOiJjMSJ9.phXN20wiOETmC5fWd3q6gY5OScI2p8owLye_ys2X3h0");
+
+        // 响应模型
+        CloseableHttpResponse response = null;
+        try {
+            // 配置信息
+            RequestConfig requestConfig = RequestConfig.custom()
+                    // 设置连接超时时间(单位毫秒)
+                    .setConnectTimeout(5000)
+                    // 设置请求超时时间(单位毫秒)
+                    .setConnectionRequestTimeout(5000)
+                    // socket读写超时时间(单位毫秒)
+                    .setSocketTimeout(5000)
+                    // 设置是否允许重定向(默认为true)
+                    .setRedirectsEnabled(true).build();
+
+            // 将上面的配置信息 运用到这个Get请求里
+            httpGet.setConfig(requestConfig);
+
+            // 由客户端执行(发送)Get请求
+            response = httpClient.execute(httpGet);
+
+            // 从响应模型中获取响应实体
+            HttpEntity responseEntity = response.getEntity();
+            System.out.println("响应状态为:" + response.getStatusLine());
+            if (responseEntity != null) {
+                System.out.println("响应内容长度为:" + responseEntity.getContentLength());
+                System.out.println("响应内容为:" + EntityUtils.toString(responseEntity));
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                // 释放资源
+                if (httpClient != null) {
+                    httpClient.close();
+                }
+                if (response != null) {
+                    response.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
